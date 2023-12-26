34163ffd10e97b1b57906206d2cbac0b464e1b08
Illia <136450403+Dokuqui@users.noreply.github.com>
2023-12-26 20:49:07 +0000

    feat: Update of plugin code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Plugin to force your version in semantic release
 Specially created custom plugin for semantic release. Goal of plugin to help you force your version if there is need. 
 
 Documentation:
-1. [Usage](../Semantic-release_Force-version-plugin/docs/Usage.md)'Usage of plugin'
-2. [Rules](../Semantic-release_Force-version-plugin/docs/Rules.md)'Restrictions of usage'
+1. [Usage](./docs/Usage.md)'Usage of plugin'
+2. [Rules](./docs/Rules.md)'Restrictions of usage'
 
 Created by Illia Semenov
--- src/force_version.js
+++ src/force_version.js
@@ -1,20 +1,41 @@
-module.exports = {
-    analyzeCommits: (pluginConfig, context) => {
-        const { FORCE_VERSION } = pluginConfig;
-        const { env } = context;
+// Local Usage
 
-        if (/^\d+\.\d+\.\d+$/.test(FORCE_VERSION)) {
-            const [major, minor, patch] = FORCE_VERSION.split(".").map(Number);
+// import SemanticReleaseError from "@semantic-release/error";
 
-            if (major > 0) {
-                return "major";
-            } else if (minor > 0) {
-                return "minor";
-            } else if (patch > 0) {
-                return "patch";
-            }
-        } else {
-            return null;
-        }
-    },
+// export async function analyzeCommits(pluginConfig, context) {
+//   const { FORCE_TYPE_VERSION } = pluginConfig;
+//   const validReleaseTypes = ["major", "minor", "patch"];
+
+//   if (FORCE_TYPE_VERSION === undefined || FORCE_TYPE_VERSION === "") {
+//     return null;
+//   } else if (!validReleaseTypes.includes(FORCE_TYPE_VERSION)) {
+//     throw new SemanticReleaseError(
+//       'Invalid FORCE_TYPE_VERSION',
+//       "EINVALIDTYPE",
+//       `Valid values of FORCE_TYPE_VERSION are ${validReleaseTypes}, but provided value: ${FORCE_TYPE_VERSION}`
+//     );
+//   }
+
+//   return FORCE_TYPE_VERSION;
+// }
+
+// CI Usage
+
+import SemanticReleaseError from "@semantic-release/error";
+
+export async function analyzeCommits(pluginConfig, context) {
+  const { env } = context;
+  const validReleaseTypes = ["major", "minor", "patch"];
+
+  if (env['FORCE_TYPE_VERSION'] === undefined || env['FORCE_TYPE_VERSION'] === "") {
+    return null;
+  } else if (!validReleaseTypes.includes(env['FORCE_TYPE_VERSION'])) {
+    throw new SemanticReleaseError(
+      'Invalid FORCE_TYPE_VERSION',
+      "EINVALIDTYPE",
+      `Valid values of FORCE_TYPE_VERSION are ${validReleaseTypes}, but provided value: ${env['FORCE_TYPE_VERSION']}`
+    );
+  }
+
+  return env['FORCE_TYPE_VERSION'];
 }
