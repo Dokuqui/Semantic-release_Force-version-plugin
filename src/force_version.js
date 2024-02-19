b7ba3bcac195c04a99d93ed99550561958afa60f
Illia <doku_isv@icloud.com>
2024-02-19 09:08:50 +0100

    chore: update dociumantations and code

--- src/force_version.js
+++ src/force_version.js
@@ -1,26 +1,3 @@
-// Local Usage
-
-// import SemanticReleaseError from "@semantic-release/error";
-
-// export async function analyzeCommits(pluginConfig, context) {
-//   const { FORCE_TYPE_VERSION } = pluginConfig;
-//   const validReleaseTypes = ["major", "minor", "patch"];
-
-//   if (FORCE_TYPE_VERSION === undefined || FORCE_TYPE_VERSION === "") {
-//     return null;
-//   } else if (!validReleaseTypes.includes(FORCE_TYPE_VERSION)) {
-//     throw new SemanticReleaseError(
-//       'Invalid FORCE_TYPE_VERSION',
-//       "EINVALIDTYPE",
-//       `Valid values of FORCE_TYPE_VERSION are ${validReleaseTypes}, but provided value: ${FORCE_TYPE_VERSION}`
-//     );
-//   }
-
-//   return FORCE_TYPE_VERSION;
-// }
-
-// CI Usage
-
 import SemanticReleaseError from '@semantic-release/error'
 
 export default async function analyzeCommits(pluginConfig, context) {
